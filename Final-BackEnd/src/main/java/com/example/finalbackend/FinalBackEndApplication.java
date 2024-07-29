7fcd16165c67111a7067c72902601f2f97a4a795
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-07-29 18:50:24 +0900

    final
    final

---
+++ Final-BackEnd/src/main/java/com/example/finalbackend/FinalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBackEndApplication.class, args);
+	}
+
+}
